9a1ae430ab7437780213297f79f8a1439047b2d7
msbevwilson <82551832+msbevwilson@users.noreply.github.com>
2025-01-13 13:53:59 -0500

    wip

--- FrontEnd/js/functions.js
+++ FrontEnd/js/functions.js
@@ -1,12 +1,14 @@
 var figureHTML = "";
+var categoryMenuHTML = "<input type='button' value='All'>";
+
 window.onload = function() {
     getData();
-    
+    getCategories();
 };
 
 async function getData() {
     try {
-      const response = await fetch('http://localhost:5678/api/works');
+      let response = await fetch('http://localhost:5678/api/works');
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
@@ -17,7 +19,24 @@ async function getData() {
       })
       document.getElementById("gallery").innerHTML = figureHTML;
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
-  }
- 
+}
+
+async function getCategories() {
+    try {
+      let response = await fetch('http://localhost:5678/api/categories');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      let categories = new Set();
+      categories = await response.json();
+
+      categories.forEach(row => {
+        categoryMenuHTML += "<input type='button' value='" + row.name + "'>";
+      })
+      document.getElementById("categoryMenu").innerHTML = categoryMenuHTML;
+    } catch (error) {
+        console.error(error);
+    }
+}
